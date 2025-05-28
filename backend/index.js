b73c9bc1675609fce3b422971b7f82600b4261b7
Sakib Ahmed <alisakib543@gmail.com>
2025-05-29 04:27:02 +0500

    comment

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import todoRoutes from './routes/todo.route.js';
 
 dotenv.config();
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 
 app.use('/api/todos', todoRoutes);
