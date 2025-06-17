70909ce96953a15f1964b2e0f6186da064065616
Sakib543 <alisakib543@gmail.com>
2025-06-17 06:54:54 +0500

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 import todoRoutes from './routes/todo.route.js';
-import cors from 'cors';
-
+// import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 app.use(express.json());    
-app.use(cors());
+// app.use(cors());
 
 app.use('/api/todos', todoRoutes);
     
 
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
